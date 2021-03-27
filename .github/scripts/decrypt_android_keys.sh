0e9494d1bf38eb9481a89825ffd61043d3dd270a
LucasG04 <lucasgolenia@gmail.com>
2021-03-27 17:47:39 +0100

    adds files for auto google deploy

---
+++ .github/scripts/decrypt_android_keys.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# --batch to prevent interactive command
+# --yes to assume "yes" for questions
+gpg --quiet --batch --yes --decrypt --passphrase="$ANDROID_KEYS_SECRET_PASSPHRASE" --output android/android_keys.zip android/android_keys.zip.gpg
+gpg --quiet --batch --yes --decrypt --passphrase="$ANDROID_KEYS_SECRET_PASSPHRASE" --output android/app/google-services.json android/app/google-services.json.gpg
+cd android
+jar xvf android_keys.zip
+cd -
