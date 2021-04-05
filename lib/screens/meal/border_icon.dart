9dcf9f3a1e25923ef8750ab8ffd304c907439eeb
LucasG04 <lucasgolenia@gmail.com>
2021-04-05 14:44:46 +0200

    adds glass effect to border icon

--- lib/screens/meal/border_icon.dart
+++ lib/screens/meal/border_icon.dart
@@ -1,33 +1,43 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
+import 'package:foodly/constants.dart';
 
 class BorderIcon extends StatelessWidget {
   final Widget child;
   final EdgeInsets padding;
   final double width, height;
+  final bool withBorder;
 
   const BorderIcon({
     Key key,
     @required this.child,
-    this.padding,
     this.width,
     this.height,
+    this.padding,
+    this.withBorder = false,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: width,
-      height: height,
-      decoration: BoxDecoration(
-        color: Colors.white,
-        borderRadius: BorderRadius.all(Radius.circular(15.0)),
-        border: Border.all(
-          color: Colors.black.withAlpha(40),
-          width: 2,
+    return new ClipRect(
+      child: BackdropFilter(
+        filter: ImageFilter.blur(sigmaX: 10.0, sigmaY: 10.0),
+        child: Container(
+          width: width,
+          height: height,
+          padding: padding ?? const EdgeInsets.all(8.0),
+          decoration: BoxDecoration(
+            color: Colors.grey.shade200.withOpacity(0.5),
+            borderRadius: BorderRadius.circular(kRadius * 2),
+            border: Border.all(
+              color: Colors.black.withAlpha(40),
+              width: withBorder ? 2.0 : 0.0,
+            ),
+          ),
+          child: Center(child: child),
         ),
       ),
-      padding: padding ?? const EdgeInsets.all(8.0),
-      child: Center(child: child),
     );
   }
 }
