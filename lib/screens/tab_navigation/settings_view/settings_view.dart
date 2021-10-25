20fc7432b38d54763da90b9ff0fa826ef26d572d
Lucas Golenia <lucas.golenia@atos.net>
2021-10-25 20:37:57 +0200

    120 - adds emergency logout in settings

--- lib/screens/tab_navigation/settings_view/settings_view.dart
+++ lib/screens/tab_navigation/settings_view/settings_view.dart
@@ -2,6 +2,7 @@ import 'package:concentric_transition/page_route.dart';
 import 'package:eva_icons_flutter/eva_icons_flutter.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:foodly/screens/tab_navigation/settings_view/loading_logout.dart';
 import 'package:modal_bottom_sheet/modal_bottom_sheet.dart';
 import 'package:share/share.dart';
 
@@ -12,7 +13,6 @@ import '../../../services/plan_service.dart';
 import '../../../services/settings_service.dart';
 import '../../../utils/basic_utils.dart';
 import '../../../widgets/page_title.dart';
-import '../../../widgets/small_circular_progress_indicator.dart';
 import '../../onboarding/onboarding_screen.dart';
 import 'help_slides/help_slide_share_import.dart';
 import 'import_meals_modal.dart';
@@ -129,7 +129,7 @@ class _SettingsViewState extends State<SettingsView> {
                 ),
               ),
             )
-          : Center(child: SmallCircularProgressIndicator());
+          : LoadingLogut();
     });
   }
 
