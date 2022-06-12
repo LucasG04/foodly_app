2eb0b3bce6d22b0da28ff0fb607a0c91a8881bb3
LucasG04 <mail4lg@yahoo.de>
2022-06-12 16:31:03 +0200

    code improvements MealScreen

---
+++ lib/screens/meal/tag_tile.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+import 'border_icon.dart';
+
+class TagTile extends StatelessWidget {
+  final String text;
+
+  const TagTile(
+    this.text, {
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.only(left: 25),
+      child: Column(
+        // ignore: avoid_redundant_argument_values
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          BorderIcon(
+            height: 50.0,
+            withBorder: true,
+            child: Text(text, style: const TextStyle(fontSize: 16.0)),
+          ),
+        ],
+      ),
+    );
+  }
+}
