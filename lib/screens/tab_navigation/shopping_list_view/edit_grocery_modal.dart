019f11eb90a510ded1b545d138ede870efb4848e
LucasG04 <mail4lg@yahoo.de>
2022-09-02 19:14:15 +0200

    improves suggestion overflow and focus change

--- lib/screens/tab_navigation/shopping_list_view/edit_grocery_modal.dart
+++ lib/screens/tab_navigation/shopping_list_view/edit_grocery_modal.dart
@@ -10,6 +10,7 @@ import '../../../services/shopping_list_service.dart';
 import '../../../utils/basic_utils.dart';
 import '../../../utils/convert_util.dart';
 import '../../../utils/debouncer.dart';
+import '../../../widgets/disposable_widget.dart';
 import '../../../widgets/main_button.dart';
 import '../../../widgets/main_text_field.dart';
 import '../../../widgets/progress_button.dart';
@@ -29,12 +30,14 @@ class EditGroceryModal extends StatefulWidget {
   State<EditGroceryModal> createState() => _EditGroceryModalState();
 }
 
-class _EditGroceryModalState extends State<EditGroceryModal> {
+class _EditGroceryModalState extends State<EditGroceryModal>
+    with DisposableWidget {
   late Debouncer _nameDebouncer;
   late bool _isCreating;
   late TextEditingController _nameController;
   late TextEditingController _amountController;
   late TextEditingController _unitController;
+  late FocusNode _nameFocusNode;
   late FocusNode _amountFocusNode;
   late FocusNode _unitFocusNode;
 
@@ -54,10 +57,25 @@ class _EditGroceryModalState extends State<EditGroceryModal> {
     _$buttonState =
         StateProvider.autoDispose<ButtonState>((_) => ButtonState.normal);
     _$suggestions = StateProvider.autoDispose<List<Grocery>>((_) => []);
+    _nameFocusNode = FocusNode();
     _amountFocusNode = FocusNode();
     _unitFocusNode = FocusNode();
 
+    _nameFocusNode.addListener(() => _onNameFocusChanged());
+
     super.initState();
+  }
+
+  @override
+  void dispose() {
+    _nameDebouncer.dispose();
+    _nameController.dispose();
+    _amountController.dispose();
+    _unitController.dispose();
+    _nameFocusNode.dispose();
+    _amountFocusNode.dispose();
+    _unitFocusNode.dispose();
+    super.dispose();
   }
 
   @override
@@ -70,86 +88,91 @@ class _EditGroceryModalState extends State<EditGroceryModal> {
       padding: EdgeInsets.symmetric(
         horizontal: (MediaQuery.of(context).size.width - width) / 2,
       ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        mainAxisSize: MainAxisSize.min,
-        children: <Widget>[
-          Consumer(builder: (_, ref, __) {
-            return SizedBox(
-                height: ref(_$suggestions).state.isNotEmpty ? kPadding : 0);
-          }),
-          Consumer(builder: (_, ref, __) {
-            return Wrap(
-              spacing: kPadding / 2,
-              runSpacing: kPadding / 2,
-              children: ref(_$suggestions).state.take(6).map((grocery) {
-                return GrocerySuggestionTile(
-                  grocery: grocery,
-                  onTap: () => _applyGroceryFromSuggestion(grocery),
-                );
-              }).toList(),
-            );
-          }),
-          const SizedBox(height: kPadding / 2),
-          Consumer(builder: (_, ref, __) {
-            ref(_$buttonState);
-            return MainTextField(
-              controller: _nameController,
-              title: 'edit_grocery_modal_ctrl_name_title'.tr(),
-              placeholder: 'edit_grocery_modal_ctrl_name_placeholder'.tr(),
-              errorText: _errorText,
-              textInputAction: TextInputAction.next,
-              onSubmit: () => _amountFocusNode.requestFocus(),
-              onChange: (text) =>
-                  _nameDebouncer.run(() => _loadSuggestions(text)),
-            );
-          }),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              SizedBox(
-                width: width * 0.4,
-                child: MainTextField(
-                  controller: _amountController,
-                  focusNode: _amountFocusNode,
-                  title: 'edit_grocery_modal_ctrl_amount_title'.tr(),
-                  placeholder: '1',
-                  keyboardType: const TextInputType.numberWithOptions(
-                    decimal: true,
-                  ),
-                  textInputAction: TextInputAction.next,
-                  onSubmit: () => _unitFocusNode.requestFocus(),
-                ),
-              ),
-              SizedBox(
-                width: width * 0.5,
-                child: MainTextField(
-                  controller: _unitController,
-                  focusNode: _unitFocusNode,
-                  title: 'edit_grocery_modal_ctrl_unit_title'.tr(),
-                  placeholder: 'edit_grocery_modal_ctrl_unit_placeholder'.tr(),
-                  onSubmit: _saveGrocery,
-                ),
-              ),
-            ],
-          ),
-          const SizedBox(height: kPadding * 2),
-          Center(
-            child: Consumer(builder: (_, ref, __) {
-              return MainButton(
-                text: 'save'.tr(),
-                isProgress: true,
-                buttonState: ref(_$buttonState).state,
-                onTap: _saveGrocery,
+      child: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            Consumer(builder: (_, ref, __) {
+              return SizedBox(
+                height: ref(_$suggestions).state.isNotEmpty ? kPadding : 0,
               );
             }),
-          ),
-          SizedBox(
-            height: MediaQuery.of(context).viewInsets.bottom == 0
-                ? kPadding * 2
-                : MediaQuery.of(context).viewInsets.bottom,
-          ),
-        ],
+            Consumer(builder: (_, ref, __) {
+              return Wrap(
+                spacing: kPadding / 2,
+                runSpacing: kPadding / 2,
+                children: ref(_$suggestions).state.take(6).map((grocery) {
+                  return GrocerySuggestionTile(
+                    grocery: grocery,
+                    onTap: () => _applyGroceryFromSuggestion(grocery),
+                  );
+                }).toList(),
+              );
+            }),
+            const SizedBox(height: kPadding / 2),
+            Consumer(builder: (_, ref, __) {
+              ref(_$buttonState);
+              return MainTextField(
+                controller: _nameController,
+                focusNode: _nameFocusNode,
+                title: 'edit_grocery_modal_ctrl_name_title'.tr(),
+                placeholder: 'edit_grocery_modal_ctrl_name_placeholder'.tr(),
+                errorText: _errorText,
+                textInputAction: TextInputAction.next,
+                onSubmit: () => _amountFocusNode.requestFocus(),
+                onChange: (text) =>
+                    _nameDebouncer.run(() => _loadSuggestions(text)),
+              );
+            }),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                SizedBox(
+                  width: width * 0.4,
+                  child: MainTextField(
+                    controller: _amountController,
+                    focusNode: _amountFocusNode,
+                    title: 'edit_grocery_modal_ctrl_amount_title'.tr(),
+                    placeholder: '1',
+                    keyboardType: const TextInputType.numberWithOptions(
+                      decimal: true,
+                    ),
+                    textInputAction: TextInputAction.next,
+                    onSubmit: () => _unitFocusNode.requestFocus(),
+                  ),
+                ),
+                SizedBox(
+                  width: width * 0.5,
+                  child: MainTextField(
+                    controller: _unitController,
+                    focusNode: _unitFocusNode,
+                    title: 'edit_grocery_modal_ctrl_unit_title'.tr(),
+                    placeholder:
+                        'edit_grocery_modal_ctrl_unit_placeholder'.tr(),
+                    onSubmit: _saveGrocery,
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(height: kPadding * 2),
+            Center(
+              child: Consumer(builder: (_, ref, __) {
+                return MainButton(
+                  text: 'save'.tr(),
+                  isProgress: true,
+                  buttonState: ref(_$buttonState).state,
+                  onTap: _saveGrocery,
+                );
+              }),
+            ),
+            SizedBox(
+              height: MediaQuery.of(context).viewInsets.bottom == 0
+                  ? kPadding * 2
+                  : MediaQuery.of(context).viewInsets.bottom,
+            ),
+          ],
+        ),
       ),
     );
   }
@@ -217,5 +240,14 @@ class _EditGroceryModalState extends State<EditGroceryModal> {
 
     context.read(_$suggestions).state = [];
     _amountFocusNode.requestFocus();
+  }
+
+  void _onNameFocusChanged() {
+    if (!_nameFocusNode.hasFocus) {
+      context.read(_$suggestions).state = [];
+    } else if (context.read(_$suggestions).state.isEmpty &&
+        _nameController.text.isNotEmpty) {
+      _nameDebouncer.run(() => _loadSuggestions(_nameController.text));
+    }
   }
 }
