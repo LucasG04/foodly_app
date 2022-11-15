6c8c4038952b96912af76c3dda8b2e29076c2a72
LucasG04 <mail4lg@yahoo.de>
2022-11-15 11:43:37 +0100

    prmium info opens get premium modal

--- lib/widgets/get_premium_info.dart
+++ lib/widgets/get_premium_info.dart
@@ -1,8 +1,11 @@
 import 'dart:math';
 
+import 'package:eva_icons_flutter/eva_icons_flutter.dart';
 import 'package:flutter/material.dart';
 
 import '../constants.dart';
+import '../utils/widget_utils.dart';
+import 'get_premium_modal.dart';
 
 class GetPremiumInfo extends StatelessWidget {
   final String title;
@@ -23,39 +26,50 @@ class GetPremiumInfo extends StatelessWidget {
     final showInfo = Random().nextDouble() <= displayProbability;
     return !showInfo
         ? const SizedBox()
-        : Container(
-            width: width,
-            decoration: BoxDecoration(
-              color: kGreyBackgroundColor,
-              borderRadius: BorderRadius.circular(kRadius),
-            ),
-            padding: const EdgeInsets.all(kPadding / 2),
-            child: Row(
-              children: [
-                const Text('⭐️', style: TextStyle(fontSize: 24)),
-                const SizedBox(width: kPadding / 2),
-                Expanded(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      Text(
-                        title,
-                        style: const TextStyle(
-                          fontSize: 16,
-                          fontWeight: FontWeight.w600,
+        : InkWell(
+            onTap: () => _openGetPremium(context),
+            child: Container(
+              width: width,
+              decoration: BoxDecoration(
+                color: kGreyBackgroundColor,
+                borderRadius: BorderRadius.circular(kRadius),
+              ),
+              padding: const EdgeInsets.all(kPadding / 2),
+              child: Row(
+                children: [
+                  const Text('⭐️', style: TextStyle(fontSize: 24)),
+                  const SizedBox(width: kPadding / 2),
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Text(
+                          title,
+                          style: const TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.w600,
+                          ),
                         ),
-                      ),
-                      const SizedBox(height: kPadding / 6),
-                      Text(
-                        description,
-                        style: Theme.of(context).textTheme.bodyText1,
-                      ),
-                    ],
+                        const SizedBox(height: kPadding / 6),
+                        Text(
+                          description,
+                          style: Theme.of(context).textTheme.bodyText1,
+                        ),
+                      ],
+                    ),
                   ),
-                )
-              ],
+                  const Icon(EvaIcons.arrowIosForwardOutline),
+                ],
+              ),
             ),
           );
+  }
+
+  void _openGetPremium(BuildContext context) {
+    WidgetUtils.showFoodlyBottomSheet<void>(
+      context: context,
+      builder: (_) => const GetPremiumModal(),
+    );
   }
 }
