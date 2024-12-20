369ab5e90929c2e3107c336fb8d38fe089380ef7
LucasG04 <mail4lg@yahoo.de>
2024-12-20 23:48:21 +0100

    adds sweep gradient to get premium setting tile

--- lib/screens/settings/settings_tile.dart
+++ lib/screens/settings/settings_tile.dart
@@ -3,13 +3,14 @@ import 'package:flutter/material.dart';
 
 import '../../../constants.dart';
 
-class SettingsTile extends StatelessWidget {
+class SettingsTile extends StatefulWidget {
   final String text;
   final IconData? leadingIcon;
   final Widget? trailing;
   final void Function()? onTap;
   final Color? colorIcon;
   final Color? colorText;
+  final bool gradientBorder;
 
   const SettingsTile({
     required this.text,
@@ -18,35 +19,94 @@ class SettingsTile extends StatelessWidget {
     this.onTap,
     this.colorIcon,
     this.colorText,
+    this.gradientBorder = false,
     Key? key,
   }) : super(key: key);
 
   @override
+  State<SettingsTile> createState() => _SettingsTileState();
+}
+
+class _SettingsTileState extends State<SettingsTile>
+    with SingleTickerProviderStateMixin {
+  AnimationController? _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    if (widget.gradientBorder) {
+      _controller = AnimationController(
+        vsync: this,
+        duration: const Duration(seconds: 4),
+      )..repeat();
+    }
+  }
+
+  @override
+  void dispose() {
+    _controller?.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
+    return _controller == null
+        ? _buildTile()
+        : AnimatedBuilder(
+            animation: _controller!,
+            child: _buildTile(),
+            builder: (context, child) {
+              return Container(
+                decoration: BoxDecoration(
+                  gradient: SweepGradient(
+                    colors: const [
+                      Colors.pink,
+                      Colors.red,
+                      Colors.purple,
+                      Colors.deepPurple,
+                      Colors.red,
+                    ],
+                    transform: GradientRotation(_controller!.value * 2 * 3.14),
+                  ),
+                  borderRadius: BorderRadius.circular(kRadius),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(2.0),
+                  child: child,
+                ),
+              );
+            },
+          );
+  }
+
+  Widget _buildTile() {
     final Color? textColor =
-        colorText ?? Theme.of(context).textTheme.bodyLarge!.color;
+        widget.colorText ?? Theme.of(context).textTheme.bodyLarge!.color;
     return InkWell(
-      onTap: onTap,
+      onTap: widget.onTap,
       child: Container(
         height: 50,
-        padding: const EdgeInsets.only(bottom: kPadding / 2),
-        decoration: BoxDecoration(borderRadius: BorderRadius.circular(kRadius)),
+        padding: const EdgeInsets.all(kPadding / 2),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(kRadius),
+          color: Theme.of(context).dialogBackgroundColor,
+        ),
         child: Row(
           children: [
-            if (leadingIcon != null)
-              Icon(leadingIcon, color: colorIcon)
+            if (widget.leadingIcon != null)
+              Icon(widget.leadingIcon, color: widget.colorIcon)
             else
               const SizedBox(),
             const SizedBox(width: kPadding),
             Expanded(
               child: AutoSizeText(
-                text,
+                widget.text,
                 style: TextStyle(color: textColor),
                 maxLines: 2,
                 overflow: TextOverflow.ellipsis,
               ),
             ),
-            trailing ?? const SizedBox(),
+            widget.trailing ?? const SizedBox(),
           ],
         ),
       ),
