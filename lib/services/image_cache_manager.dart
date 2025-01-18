2721a0240710c2f99f89bac09a5a2ecab54cafa7
LucasG04 <mail4lg@yahoo.de>
2025-01-18 17:02:08 +0100

    rewrites image cache manager to save strings instead of Uint8List

--- lib/services/image_cache_manager.dart
+++ lib/services/image_cache_manager.dart
@@ -1,6 +1,8 @@
 import 'dart:collection';
+import 'dart:convert'; // Für die Base64-Kodierung und -Dekodierung
 import 'dart:typed_data';
 
+import 'package:firebase_performance/firebase_performance.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:logging/logging.dart';
 
@@ -14,13 +16,13 @@ class ImageCacheManager {
   static final LinkedHashMap<String, DateTime> accessTimeMap = LinkedHashMap();
 
   static Future<void> initialize() async {
-    const boxName = 'imageCache';
+    imageCache = await Hive.openBox('imageCache2');
+
+    // delete old image box
     try {
-      imageCache = await Hive.openBox(boxName);
+      await Hive.deleteBoxFromDisk('imageCache');
     } catch (e) {
-      _log.severe('Failed to open $boxName box: $e');
-      await Hive.deleteBoxFromDisk(boxName);
-      imageCache = await Hive.openBox(boxName);
+      _log.fine('Could not delete old imageCache box', e);
     }
   }
 
@@ -28,20 +30,26 @@ class ImageCacheManager {
     _evictExpiredItems(); // Evict expired items before returning the cached image
     if (imageCache.containsKey(url)) {
       accessTimeMap[url] = DateTime.now();
-      return imageCache.get(url);
+      final String base64String = imageCache.get(url);
+      return base64Decode(base64String);
     }
     return null;
   }
 
   static void put(String url, Uint8List imageBytes) {
+    final timeTrace =
+        FirebasePerformance.instance.newTrace('ImageCacheManager-put');
+    timeTrace.start();
     _evictExpiredItems(); // Evict expired items before adding a new one
 
     if (imageCache.length >= maxCacheSize) {
       _evictLRUItem(); // Evict LRU if cache is full
     }
 
-    imageCache.put(url, imageBytes);
+    final String base64String = base64Encode(imageBytes);
+    imageCache.put(url, base64String);
     accessTimeMap[url] = DateTime.now();
+    timeTrace.stop();
   }
 
   /// Evict the least recently used item
